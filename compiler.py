b1896c782b4383f58b03270252da3495d64e3807
David Martinez <34782922+dmtzt@users.noreply.github.com>
2022-11-17 15:14:27 -0600

    refactor(compiler): rename invalid file extension error

--- compiler.py
+++ compiler.py
@@ -4,7 +4,6 @@ from pathlib import Path
 from compiler.files import FileReader
 from compiler.files import FilePrinter
 from compiler.parser import Parser
-from compiler.quadruples import QuadrupleList
 
 def main():
     FILE_EXTENSION = '.epo'
@@ -28,25 +27,20 @@ def main():
     fsuffix = file_reader.get_file_suffix(fpath)
 
     if fsuffix != FILE_EXTENSION:
-        raise IncorrectFileExtensionError()
+        raise InvalidFileExtensionError()
 
     fdata = file_reader.read_file(fpath)
     quadruple_list = parser.parse(fdata)
 
     if args.names:
-        print('Names')
         named_repr_fpath = file_printer.generate_named_representation_file_path(fstem)
         file_printer.generate_named_representation_file(named_repr_fpath, quadruple_list)
 
-    print('Code')
     code_repr_fpath = file_printer.generate_intermediate_code_representation_file_path(fstem)
     file_printer.generate_intermediate_code_representation_file(code_repr_fpath, quadruple_list)
-    # print(fpath)
-    # print(fstem)
-    # print(fsuffix)
 
 
-class IncorrectFileExtensionError(RuntimeError):
+class InvalidFileExtensionError(RuntimeError):
     pass
 
 

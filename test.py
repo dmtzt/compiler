6a8e4412ada80db65ca80fd688aa36f010eeca0f
David Martinez <34782922+dmtzt@users.noreply.github.com>
2022-11-20 19:03:32 -0600

    test(parser): miscellaneous tests

---
+++ test.py
@@ -0,0 +1,14 @@
+from compiler.memory import GlobalActivationRecord, LocalActivationRecord, ConstantTableActivationRecord
+from compiler.memory import Memory
+
+from compiler.variables import Type
+
+# g = GlobalActivationRecord()
+# l = LocalActivationRecord()
+# c = ConstantTable()
+
+# print(g)
+# print(l)
+# print(c)
+
+print(Memory.type_dict)

db08125f4093e1cfb6c3c2efcb0c79a8e143ef49
David Martinez <34782922+dmtzt@users.noreply.github.com>
2022-11-21 20:49:26 -0600

    feat(parser)!: intermediate code container that comprises function directory and quadruple list

--- compiler/intermediate_code.py
+++ compiler/intermediate_code.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 
 from .functions import FunctionDirectory
@@ -7,3 +8,16 @@ from .quadruples import QuadrupleList
 class IntermediateCodeContainer:
     _function_directory: FunctionDirectory
     _quadruple_list : QuadrupleList
+
+    def get_intermediate_code_representation(self) -> str:
+        data = {
+            "function_directory": self._function_directory.get_intermediate_code_representation(),
+            "quadruple_list": self._quadruple_list.get_intermediate_code_representation()
+        }
+
+        json_str = json.dumps(data, indent=4)
+        return json_str
+    
+
+    def get_quadruple_list(self) -> QuadrupleList:
+        return self._quadruple_list
